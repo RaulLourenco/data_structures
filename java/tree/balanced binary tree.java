f8911b07d1e8cf423f46140089a646f924b2e5e4
Raul Lourenco <raulourenco@hotmail.com>
2024-03-14 12:08:19 -0300

    another solution for balanced binary tree + comments

--- java/tree/balanced binary tree.java
+++ java/tree/balanced binary tree.java
@@ -1,4 +1,4 @@
-O(N) Time
+O(N) Time - DFS calcuting height for each current node
 O(h) Space - height of the tree for DFS algo
 
 class Solution {
@@ -15,6 +15,33 @@ class Solution {
         int left = height(root.left);
         int right = height(root.right);
         if(left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+        return 1 + Math.max(left, right);
+    }
+}
+
+O(n) Time - bottom up approach
+O(n) Space
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int height(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int left = height(root.left);
+        int right = height(root.right);
+
         return 1 + Math.max(left, right);
     }
 }
