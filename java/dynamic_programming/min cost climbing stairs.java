48d0d64f2cef174142f8a05ca32a6b388a1c4f75
Raul Lourenco <raulourenco@hotmail.com>
2024-03-20 17:18:09 -0300

    min cost climbing stairs

---
+++ java/dynamic_programming/min cost climbing stairs.java
@@ -0,0 +1,30 @@
+//O(n) Time & Space - where n is the number of cost elements
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+}
+
+//O(n) Time - O(1) Space
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int first = cost[0];
+        int second = cost[1];
+        for(int i = 2; i < n; i++) {
+            int curr = cost[i] + Math.min(first, second);
+            first = second;
+            second = curr;
+        }
+        return Math.min(first, second);
+    }
+}
