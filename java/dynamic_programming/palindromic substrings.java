00f9b813997ad90a9409b9269e9f1384c3f5a9cc
Raul Lourenco <raulourenco@hotmail.com>
2024-03-21 13:01:54 -0300

    palindromic substrings

---
+++ java/dynamic_programming/palindromic substrings.java
@@ -0,0 +1,19 @@
+//O(n^2) Time - O(1) Space - where n is the size of string s
+//Expanding around center
+class Solution {
+    int count = 0;
+    public int countSubstrings(String s) {
+        for(int i = 0; i <  s.length(); i++) {
+           expandFromCenter(s, i, i);
+           expandFromCenter(s, i, i + 1);
+        }
+        return count;
+    }
+    private void expandFromCenter(String s, int left, int right) {
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            count++;
+        }
+    }
+}
