fe27251a14ea2519b333483d62c7a3270c31612e
Raul Lourenco <raulourenco@hotmail.com>
2024-03-22 12:51:07 -0300

    different approaches using DP for word break - using substring and starting at the beginning

--- java/dynamic_programming/word break.java
+++ java/dynamic_programming/word break.java
@@ -17,3 +17,39 @@ class Solution {
         return dp[0];
     }
 }
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[n] = true;
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(String word : wordDict) {
+                if((i + word.length()) <= s.length() && s.substring(i, i + word.length()).equals(word)) {
+                    dp[i] = dp[i + word.length()];
+                }
+                if(dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+}
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for(int i = 1; i < n + 1; i++) {
+            for(String word : wordDict) {
+                if((i - word.length() >= 0) && dp[i - word.length()] && s.substring(i - word.length(), i).equals(word)) {
+                    dp[i] = dp[i - word.length()];
+                }
+                if(dp[i]) break;
+            }
+        }
+        return dp[n];
+    }
+}
