f29d9ef16cb3b7c2a9fe95010183e7eb8c8c1d75
Raul Lourenco <raulourenco@hotmail.com>
2024-03-23 13:55:00 -0300

    diff implementation using hashmap in Single Number - bit manipulation

--- java/array/single number.java
+++ java/array/single number.java
@@ -18,6 +18,29 @@ class Solution {
     }
 }
 
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> curr : map.entrySet()) {
+            int key = curr.getKey();
+            int val = curr.getValue();
+            if(val == 1) {
+                return key;
+            }
+        }
+        return 0;
+    }
+}
+
 Bitwise XOR
 O(N) Time
 O(1) Space
