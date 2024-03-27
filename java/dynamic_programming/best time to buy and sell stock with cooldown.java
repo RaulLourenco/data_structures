b383c60746be8f62cd7458216c295f655728cc9c
Raul Lourenco <raulourenco@hotmail.com>
2024-03-27 10:18:02 -0300

    best time to buy and sell stock with cooldown

---
+++ java/dynamic_programming/best time to buy and sell stock with cooldown.java
@@ -0,0 +1,47 @@
+//DP with Top-Down approach (Memoization).
+//O(n) Time & Space - where n is the prices array
+class Solution {
+    public int maxProfit(int[] prices) {
+        Map<String, Integer> memo = new HashMap<>();
+        return max(prices, 0, true, memo);
+    }
+    private int max(int[] prices, int i, boolean buying, Map<String, Integer> memo) {
+        if(i >= prices.length) {
+            return 0;
+        }
+
+        String key = i + "-" + buying;
+
+        if(memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        int cooldown = max(prices, i + 1, buying, memo);
+        int buyOrSell = 0;
+
+        if(buying) {
+            buyOrSell = max(prices, i + 1, !buying, memo) - prices[i];
+        } else {
+            buyOrSell = max(prices, i + 2, !buying, memo) + prices[i];
+            
+        }
+        memo.put(key, Math.max(buyOrSell, cooldown));
+
+        return memo.get(key);
+    }
+}
+
+//State Machine
+//O(n) Time - O(1) Space - where n is the prices array
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sold = 0, hold = Integer.MIN_VALUE, rest = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            hold = Math.max(hold, rest - prices[i]);
+            rest = Math.max(rest, sold);
+            sold = hold + prices[i];
+        }
+        return Math.max(sold, rest);
+    }
+}
