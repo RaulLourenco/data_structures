5c79c489b4f9fbbaa8a5edf98285da1364975d61
Raul Lourenco <raulourenco@hotmail.com>
2024-04-05 11:18:03 -0300

    find median data stream

---
+++ java/heap_priorityqueue/find median data stream.java
@@ -0,0 +1,54 @@
+//O(log(n)) Time & O(n) Space
+class MedianFinder {
+    PriorityQueue<Integer> small;
+    PriorityQueue<Integer> large;
+    public MedianFinder() {
+        large = new PriorityQueue<>((a, b) -> a - b);
+        small = new PriorityQueue<>((a,b) -> b - a);
+    }
+    
+    //O(log(n)) Time
+    public void addNum(int num) {
+        small.offer(num);
+        if(small.size() > large.size() + 1 || !large.isEmpty() && small.peek() > large.peek()) {
+            int val = small.poll();
+            large.offer(val);
+        }
+        if(large.size() > small.size() + 1) {
+            int val = large.poll();
+            small.offer(val);
+        }
+    } 
+    
+    //O(1) Time
+    public double findMedian() {
+        if(small.size() > large.size()) {
+            return (double) small.peek();
+        } else if(large.size() > small.size()) {
+            return (double) large.peek();
+        }
+        return (double) (small.peek() + large.peek()) / 2.0;
+    }
+}
+
+//Brute Force (18 / 21 testcases passed) - Resulting in TLE
+class MedianFinder {
+    List<Integer> list;
+    public MedianFinder() {
+        list = new ArrayList<>();
+    }
+    
+    public void addNum(int num) {
+        list.add(num);
+        Collections.sort(list);
+    }
+    
+    public double findMedian() {
+        int mid = list.size() / 2;
+        if(list.size() % 2 == 0) {
+            return (double) (list.get(mid-1) + list.get(mid)) / 2.0;
+        } else {
+            return (double) list.get(mid);
+        }
+    }
+}
