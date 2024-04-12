3e57c8bd4bb2cd9a35348b480aeb3ba490563111
Raul Lourenco <raulourenco@hotmail.com>
2024-04-12 12:46:11 -0300

    adding a folder to sort algorithms

---
+++ java/sorts/bubble sort.java
@@ -0,0 +1,37 @@
+//O(n^2) Time & O(1) Space - where n is the length of the array
+public int[] bubbleSort(int[] array) {
+    int n = array.length;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+//O(n^2) Time & O(1) Space - where n is the length of the array
+//Optimized way to stop if didn't happened any swap
+public int[] bubbleSort(int[] array) {
+    int n = array.length;
+    boolean swapped = false;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if(!swapped) {
+            break;
+        }
+    }
+    return array;
+}
